30402b2193509e7000ba061e2771adc1cee4c474
Alex K <kumik_al@yahoo.ca>
2018-06-16 16:52:18 -0700

    Revised the leaderboard

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   get '/' => 'welcome#index', as: 'root'
-  get '/leaderboard' => 'users#leaderboard'
+  get '/leaderboard' => 'users#leaderboard' #we might change this route later and add more features
   resources :users, only: [:new, :create] 
   resource :session, only: [:new, :destroy, :create]
   resources :drill_groups
