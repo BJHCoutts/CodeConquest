40ddfd9ac796df2f42443bf3acac32aee1692d0a
Sunny Heo <mobileheo@gmail.com>
2018-06-16 14:08:32 -0700

    Change controller session to sessions

--- config/routes.rb
+++ config/routes.rb
@@ -2,16 +2,9 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   get '/' => 'welcome#index', as: 'root'
-<<<<<<< HEAD
-  resources :users, only: [:new, :create]
-  resource :session, only: [:new, :create, :destroy]
 
-  #Drill Routes
-=======
->>>>>>> 0458fe21a67f562030725249a21661d376246ada
-  
   resources :users, only: [:new, :create]
-  resource :sessions, only: [:new, :destroy, :create]
+  resource :session, only: [:new, :destroy, :create]
   resources :drill_groups
   resources :drills
   
