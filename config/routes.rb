5136576db4655fa65899b0f7c2cec2ee0b064d98
Sunny Heo <mobileheo@gmail.com>
2018-06-16 14:24:29 -0700

    Drill group controller (#15)
    * Add UserModel
    
    * Add model 'DrillGroup' & Create one-to-many relationship between user and drill_group
    
    * Add model 'Drill' & Create one-to-many association between drill_group and drills & Add gems for development
    
    * Add joint-table Take that has many-to-many raltionship between users and drills-groups
    
    * Add 'dependent: :nullify' in tables that have  has_many
    
    * Add bcrypt and validations for model 'User'
    
    * start of welcome page and basic styles
    
    * Add validtion for model drill group
    
    * Add validtion for model drill
    
    * Created Drill new, create
    
    * created users_controller and set up new action
    
    * Build basic actions for drill_group_controller
    
    * Complete DrillGorup#new & Add find_drill_group and drill_group_params
    
    * Fixed bugs and added jquery gem
    
    * Add routes for drill_group_controller & Implement basic functionality of create action
    
    * set up and tested new and create actions
    
    * Add gem 'simple_form'
    
    * moved the resources:users line a bit lower for better formant
    
    * Integrate simple form with bootstrap & Complete basic form for creating drill_group
    
    * Drill group controller (#11)
    
    * Add gem 'simple_form'
    
    * Integrate simple form with bootstrap & Complete basic form for creating drill_group
    
    * Started on sessions controller still in process of finishing
    
    * Fix a bug that doesn't create new drill group
    
    * Comeplete drill_group#create
    
    * Add gem 'cowsay'
    
    * Sessions controller has been implemented but need to populate some data in order to procesd with testing. needst to be done: controller implementation, helper methods and integration with existing data
    
    * Create basic seed for user and drill_group
    
    * Add gem 'cancancan' & Add User authentication in controller application
    
    * Add cancan:ability
    
    * Remove comments on cancan:ablitiy
    
    * Create sessions controller & Add authentication for drill_group controller & Refactor user create page with simple_form_for
    
    * Drill group controller (#13)
    
    * Add gem 'simple_form'
    
    * Integrate simple form with bootstrap & Complete basic form for creating drill_group
    
    * Fix a bug that doesn't create new drill group
    
    * Comeplete drill_group#create
    
    * Add gem 'cowsay'
    
    * Create basic seed for user and drill_group
    
    * Add gem 'cancancan' & Add User authentication in controller application
    
    * Add cancan:ability
    
    * Remove comments on cancan:ablitiy
    
    * Create sessions controller & Add authentication for drill_group controller & Refactor user create page with simple_form_for
    
    * Add view for creating session
    
    * Change controller session to sessions

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,9 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   get '/' => 'welcome#index', as: 'root'
-  
+
   resources :users, only: [:new, :create]
-  resource :sessions, only: [:new, :destroy, :create]
+  resource :session, only: [:new, :destroy, :create]
   resources :drill_groups
   resources :drills
   
