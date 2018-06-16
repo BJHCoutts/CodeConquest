e52c5a0e03a0eef7d90b7957ed41a33f8508aaee
Alex K <kumik_al@yahoo.ca>
2018-06-16 12:23:55 -0700

    moved the resources:users line a bit lower for better formant

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,12 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   get '/' => 'welcome#index', as: 'root'
-  resources :users, only: [:new, :create]
-
+  
   #Drill Routes
   
   resources :drill_groups
   resources :drills
+  resources :users, only: [:new, :create]
+  
 
 end
