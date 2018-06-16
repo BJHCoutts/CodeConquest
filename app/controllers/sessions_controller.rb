e4a7fa0e5e48e516259bbf97c765c6a3c4f324d3
Sunny Heo <mobileheo@gmail.com>
2018-06-16 15:40:55 -0700

    Fix bugs on sessions#create

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,9 +3,10 @@ class SessionsController < ApplicationController
   end
 
   def create
-    @user = User.find_by_email params[:email]
-    if @user && @user.authenticate(params[:password])
-      session[:user_id] = @user.id
+    user = User.find_by_email(params[:session][:email])
+    
+    if user&.authenticate(params[:session][:password])
+      session[:user_id] = user.id
       redirect_to root_path, notice: "Logged in"
     else
       flash[:alert] = "Wrong email or password"
