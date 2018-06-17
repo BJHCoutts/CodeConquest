a40dedf189f36df3aabb50ca4615ac3fe7bf2c09
Sunny Heo <mobileheo@gmail.com>
2018-06-17 04:01:03 -0700

    Implment create action for transcripts_controller

--- app/controllers/transcripts_controller.rb
+++ app/controllers/transcripts_controller.rb
@@ -1,19 +1,20 @@
 class TranscriptsController < ApplicationController
 
   def create
-    byebug
-    @drill = Drill.find(params[:drill_id])
-    @questions = @drill.questions 
+    drill = Drill.find(params[:drill_id])
+    questions = drill.questions 
     total_score = 0
-    @questions.each_with_index do |q, index|
+    questions.each_with_index do |q, index|
       if q.answer == params["#{index}"]
-        total_score += q.point
+        total_score += q.point.to_i
       end
     end
+    transcript = Transcript.create(user: current_user, drill: drill, score: total_score)
     
+    if transcript.save
+      redirect_to root_path
+    end
     
-    # vote = Vote.new(user: current_user, answer: answer, up: params[:up])
-    @transscript = Transcript.create(transcript_params)
   end
 
   private
