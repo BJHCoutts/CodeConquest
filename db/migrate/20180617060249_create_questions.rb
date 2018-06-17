c8d59f1cd5031b8888c443630756a163ba9eb1b9
Sunny Heo <mobileheo@gmail.com>
2018-06-16 23:04:09 -0700

    Recreate question model

--- db/migrate/20180617060249_create_questions.rb
+++ db/migrate/20180617060249_create_questions.rb
@@ -2,6 +2,7 @@ class CreateQuestions < ActiveRecord::Migration[5.2]
   def change
     create_table :questions do |t|
       t.string :body
+      t.string :answer
       t.string :options
       t.string :point
       t.belongs_to :drill, foreign_key: true
