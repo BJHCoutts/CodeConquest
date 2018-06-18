6b50b88a0255727ced67862da1eba0df4897ad5c
Sunny Heo <mobileheo@gmail.com>
2018-06-18 00:50:28 -0700

    Change type of anser in question model to be integer & Fix bug on transcript show page

--- db/migrate/20180617060249_create_questions.rb
+++ db/migrate/20180617060249_create_questions.rb
@@ -2,7 +2,7 @@ class CreateQuestions < ActiveRecord::Migration[5.2]
   def change
     create_table :questions do |t|
       t.string :body
-      t.string :answer
+      t.integer :answer
       t.string :options, array: true, default: []
       t.integer :point
       t.belongs_to :drill, foreign_key: true
